118034675c437d4a5634af1c16702429f56d8e60
kutse_0kfg5db <ivankucevalov@gmail.com>
2023-02-22 15:25:03 +0200

    home-task 16 commit

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from newapp.views import view, author_details, AuthorListView, PublisherListView, BookListView, StoreListView, \
-    publisher_details, book_details, store_details
+from newapp.views import view, AuthorListView, PublisherListView, BookListView, StoreListView, \
+    AuthorDetails, StoreDetail, BookDetails, PublisherDetails, BookUpdateView, AuthorUpdateView, \
+    PublisherUpdateView, StoreUpdateView, BookDelete, AuthorDelete, PublisherDelete, StoreDelete, BookCreate, \
+    AuthorCreate, PublisherCreate, StoreCreate
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,8 +31,20 @@ urlpatterns = [
     path('pyblisher/', PublisherListView.as_view(), name='publisher_list'),
     path('book/', BookListView.as_view(), name='book_list'),
     path('store/', StoreListView.as_view(), name='store_list'),
-    path('author/details/<int:pk>', author_details, name='author_details'),
-    path('publisher/details/<int:pk>', publisher_details, name='publisher_details'),
-    path('book/detais/<int:pk>', book_details, name='book_details'),
-    path('store/details/<int:pk>', store_details, name='store_details')
+    path('author/details/<int:pk>', AuthorDetails.as_view(), name='author_details'),
+    path('author/details/<int:pk>/update', AuthorUpdateView.as_view(), name='author_update'),
+    path('author/details/<int:pk>/delete', AuthorDelete.as_view(), name='author_delete'),
+    path('author/create/', AuthorCreate.as_view(), name='author_create'),
+    path('publisher/details/<int:pk>', PublisherDetails.as_view(), name='publisher_details'),
+    path('publisher/details/<int:pk>/update', PublisherUpdateView.as_view(), name='publisher_update'),
+    path('publisher/details/<int:pk>/delete', PublisherDelete.as_view(), name='publisher_delete'),
+    path('publisher/create/', PublisherCreate.as_view(), name='publisher_create'),
+    path('book/detais/<int:pk>', BookDetails.as_view(), name='book_details'),
+    path('book/details/<int:pk>/update', BookUpdateView.as_view(), name='book_update'),
+    path('book/details/<int:pk>/delete', BookDelete.as_view(), name='book_delete'),
+    path('book/create/', BookCreate.as_view(), name='book_create'),
+    path('store/details/<int:pk>', StoreDetail.as_view(), name='store_details'),
+    path('store/details/<int:pk>/update', StoreUpdateView.as_view(), name='store_update'),
+    path('store/details/<int:pk>/delete', StoreDelete.as_view(), name='store_delete'),
+    path('store/create/', StoreCreate.as_view(), name='store_create'),
 ]
--- mysite/newapp/views.py
+++ mysite/newapp/views.py
@@ -1,6 +1,9 @@
+from django.contrib import messages
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
 from django.views import generic
-from django.db.models import Avg, Count, FloatField, Max
+from django.db.models import Count
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from newapp.models import Author, Publisher, Book, Store
 
@@ -12,75 +15,202 @@ def view(request):
 class AuthorListView(generic.ListView):
     template_name = 'author_list.html'
     context_object_name = 'author_list'
+    paginate_by = 50
     model = Author
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['author_list'] = Author.objects.all()
-        # context['count'] = context['author_list'].count()
-        context['count'] = Author.objects.prefetch_related('books').annotate(books_count=Count('books'))[0].books_count
-
-        return context
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['author_list'] = Author.objects.all()
+    #     # context['count'] = context['author_list'].count()
+    #    context['count'] = Author.objects.prefetch_related('books').annotate(books_count=Count('books'))[0].books_count
+    #
+    #     return context
 
 
 class PublisherListView(generic.ListView):
     template_name = 'publisher_list.html'
     context_object_name = 'publisher_list'
+    paginate_by = 50
     model = Publisher
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['publisher_list'] = Publisher.objects.all()
-        # context['publisher_count'] = context['publisher_list'].count()
-        context['count'] = \
-            Publisher.objects.prefetch_related('books').annotate(books_count=Count('books'))[0].books_count
-
-        return context
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['publisher_list'] = Publisher.objects.all()
+    #     # context['publisher_count'] = context['publisher_list'].count()
+    #     context['count'] = \
+    #         Publisher.objects.prefetch_related('books').annotate(books_count=Count('books'))[0].books_count
+    #
+    #     return context
 
 
 class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 50
     template_name = 'book_list.html'
     context_object_name = 'book_list'
-    model = Book
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['book_list'] = Book.objects.all()
-        # context['publisher'] = Book.objects.select_related('publisher').all()
-        # context['book_count'] = context['book_list'].count()
-        return context
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['book_list'] = Book.objects.all()
+    #     # context['publisher'] = Book.objects.select_related('publisher').all()
+    #     # context['book_count'] = context['book_list'].count()
+    #     return context
 
 
 class StoreListView(generic.ListView):
     template_name = 'store_list.html'
     context_object_name = 'store_list'
+    paginate_by = 50
     model = Store
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['store_list'] = Store.objects.all()
-        # context['store_count'] = context['store_list'].count()
-        return context
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['store_list'] = Store.objects.all()
+    #     # context['store_count'] = context['store_list'].count()
+    #     return context
 
 
-def author_details(request, pk):
-    author = get_object_or_404(Author, pk=pk)
-    author_num = Author.objects.all().aggregate(Max('id'))['id__max']
-    return render(request, 'author_details.html', {'author': author, 'author_num': author_num})
+# def author_details(request, pk):
+#     author = get_object_or_404(Author, pk=pk)
+#     author_num = Author.objects.all().aggregate(Max('id'))['id__max']
+#     return render(request, 'author_details.html', {'author': author, 'author_num': author_num})
 
 
-def publisher_details(request, pk):
-    publisher = get_object_or_404(Publisher, pk=pk)
-    avg_price_of_published_books = Publisher.objects.annotate(Avg('books__price')).filter(pk=pk)[0].books__price__avg
-    return render(request, 'publisher_details.html', {'publisher': publisher,
-                                                      'avg_price_of_published_books': avg_price_of_published_books})
+class AuthorDetails(generic.DetailView):
+    model = Author
+    template_name = 'author_details.html'
 
 
-def book_details(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    return render(request, 'book_details.html', {'book': book})
+# def publisher_details(request, pk):
+#     publisher = get_object_or_404(Publisher, pk=pk)
+#     avg_price_of_published_books = Publisher.objects.annotate(Avg('books__price')).filter(pk=pk)[0].books__price__avg
+#     return render(request, 'publisher_details.html', {'publisher': publisher,
+#                                                       'avg_price_of_published_books': avg_price_of_published_books})
 
 
-def store_details(request, pk):
-    store = get_object_or_404(Store, pk=pk)
-    return render(request, 'store_details.html', {'store': store})
+class PublisherDetails(generic.DetailView):
+    model = Publisher
+    template_name = 'publisher_details.html'
+    queryset = Publisher.objects.prefetch_related('books').annotate(books_count=Count('books'))
+
+
+# def book_details(request, pk):
+#     book = get_object_or_404(Book, pk=pk)
+#     return render(request, 'book_details.html', {'book': book})
+
+
+class BookDetails(generic.DetailView):
+    model = Book
+    template_name = 'book_details.html'
+
+
+# def store_details(request, pk):
+#     store = get_object_or_404(Store, pk=pk)
+#     return render(request, 'store_details.html', {'store': store})
+
+
+class StoreDetail(generic.DetailView):
+    model = Store
+    template_name = 'store_details.html'
+
+
+class BookUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Book
+    fields = ['name', 'price', 'rating', 'pubdate', 'publisher']
+    template_name = 'book_form.html'
+    success_url = reverse_lazy('book_list')
+
+
+class AuthorUpdateView(generic.UpdateView):
+    model = Author
+    fields = ['name', 'age']
+    template_name = 'author_form.html'
+    success_url = reverse_lazy('author_list')
+
+
+class PublisherUpdateView(generic.UpdateView):
+    model = Publisher
+    fields = ['name']
+    template_name = 'publisher_form.html'
+    success_url = reverse_lazy('publisher_list')
+
+
+class StoreUpdateView(generic.UpdateView):
+    model = Store
+    fields = ['name']
+    template_name = 'store_form.html'
+    success_url = reverse_lazy('store_list')
+
+
+class BookDelete(LoginRequiredMixin, generic.DeleteView):
+    model = Book
+    context_object_name = 'book'
+    success_url = reverse_lazy('book_list')
+    template_name = 'book_confirm_delete.html'
+
+    def form_valid(self, form):
+        messages.success(self.request, "The task was deleted successfully.")
+        return super(BookDelete, self).form_valid(form)
+
+
+class AuthorDelete(generic.DeleteView):
+    model = Author
+    context_object_name = 'author'
+    success_url = reverse_lazy('author_list')
+    template_name = 'author_confirm_delete.html'
+
+    def form_valid(self, form):
+        messages.success(self.request, "The task was deleted successfully.")
+        return super(AuthorDelete, self).form_valid(form)
+
+
+class PublisherDelete(generic.DeleteView):
+    model = Publisher
+    context_object_name = 'publisher'
+    success_url = reverse_lazy('publisher_list')
+    template_name = 'publisher_confirm_delete.html'
+
+    def form_valid(self, form):
+        messages.success(self.request, "The task was deleted successfully.")
+        return super(PublisherDelete, self).form_valid(form)
+
+
+class StoreDelete(generic.DeleteView):
+    model = Store
+    context_object_name = 'store'
+    success_url = reverse_lazy('store_list')
+    template_name = 'store_confirm_delete.html'
+
+    def form_valid(self, form):
+        messages.success(self.request, "The task was deleted successfully.")
+        return super(StoreDelete, self).form_valid(form)
+
+
+class BookCreate(LoginRequiredMixin, generic.CreateView):
+    model = Book
+    fields = ['name', 'price', 'rating', 'pubdate', 'publisher', 'authors']
+    template_name = 'book_form.html'
+    success_url = reverse_lazy('book_list')
+
+
+class AuthorCreate(generic.CreateView):
+    model = Author
+    fields = ['name', 'age']
+    template_name = 'author_form.html'
+    success_url = reverse_lazy('author_list')
+
+
+class PublisherCreate(generic.CreateView):
+    model = Publisher
+    fields = ['name']
+    template_name = 'publisher_form.html'
+    success_url = reverse_lazy('publisher_list')
+
+
+class StoreCreate(generic.CreateView):
+    model = Store
+    fields = ['name', 'books']
+    template_name = 'store_form.html'
+    success_url = reverse_lazy('store_list')
+
+
